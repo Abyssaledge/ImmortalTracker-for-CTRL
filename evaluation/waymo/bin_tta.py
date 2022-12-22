33e1a6185d8239ecfacc89121de1fd555305117b
Abyssaledge <1060056270@qq.com>
2022-12-22 20:50:35 +0800

    finish basic TTA

--- evaluation/waymo/bin_tta.py
+++ evaluation/waymo/bin_tta.py
@@ -5,6 +5,9 @@ from tqdm import tqdm
 from ipdb import set_trace
 from mot_3d.tracklet import SimpleTracklet
 import numpy as np
+import os
+from os import path as osp
+import argparse, yaml
 
 def read_bin(file_path):
     with open(file_path, 'rb') as f:
@@ -21,7 +24,7 @@ def generate_tracklets(bin_data):
         seg_name = o.context_name
         obj_uuid = seg_name + '-' + obj_id
         box = o.object.box
-        box_np = np.array([box.center_x, box.center_y, box.center_z, box.width, box.length, box.height, box.heading], dtype=np.float32)
+        box_np = np.array([box.center_x, box.center_y, box.center_z, box.width, box.length, box.height, box.heading, o.score], dtype=np.float32)
         cat = o.object.type
         if obj_uuid not in tracklets:
             new_tracklet = SimpleTracklet(obj_uuid, cat)
@@ -41,27 +44,125 @@ def generate_tracklets(bin_data):
 
     return tracklets_seg_dict
 
-def match_tracklets(trks1, trks2, mode, cfg):
+def match_tracklets(trks1, trks2, cfg):
     cost = np.zeros((len(trks1), len(trks2)))
     for i in range(len(trks1)):
         for k in range(len(trks2)):
-            dis = SimpleTracklet.tracklet_distance(trks1[i], trks2[k], mode=mode, cfg=cfg)
+            dis = SimpleTracklet.tracklet_distance(trks1[i], trks2[k], cfg=cfg)
             cost[i, k] = dis
-    set_trace()
+    argmin = cost.argmin(1)
+    min_cost = cost.min(1)
+    matched_min = min_cost < cfg['cost_thresh']
+    matched_pairs = []
+    for i in range(len(argmin)):
+        if matched_min[i].item():
+            pair = (i, argmin[i].item())
+            matched_pairs.append(pair)
 
+
+    return matched_pairs
+
+def merge_tracklets(trks1, trks2, pairs, cfg):
+
+    out_trks = []
+
+    if cfg.get('keep_unmatch1', False):
+        if len(pairs) == 0:
+            out_trks = trks1
+        else:
+            indices_1 = np.array([p[0] for p in pairs], dtype=np.int)
+            unmatched_1 = np.ones(indices_1.max().item() + 1, dtype=bool)
+            unmatched_1[indices_1] = False
+            unmatched_1 = np.where(unmatched_1)[0].tolist()
+            out_trks += [trks1[i] for i in unmatched_1]
+
+    if cfg.get('keep_unmatch2', False):
+
+        max_id_1 = max([t.int_id for t in trks1])
+        for t2 in trks2:
+            t2.increase_id(max_id_1 + 1)
+
+        if len(pairs) == 0:
+            out_trks += trks2
+        else:
+            indices_2 = np.array([p[1] for p in pairs], dtype=np.int)
+            unmatched_2 = np.ones(indices_2.max().item() + 1, dtype=bool)
+            unmatched_2[indices_2] = False
+            unmatched_2 = np.where(unmatched_2)[0].tolist()
+            out_trks += [trks2[i] for i in unmatched_2]
+    
+
+    for pair in pairs:
+        merged_trk = SimpleTracklet.merge_tracklets([trks1[pair[0]], trks2[pair[1]]], cfg)
+        out_trks.append(merged_trk)
+    return out_trks
+
+def save_to_bin(tracklet_dict, save_path):
+    objects = metrics_pb2.Objects()
+    for seg_name, trks in tracklet_dict.items():
+        for trk in trks:
+            waymo_objs = trk.waymo_objects()
+            for obj in waymo_objs:
+                objects.objects.append(obj)
+
+    f = open(save_path, 'wb')
+    f.write(objects.SerializeToString())
+    f.close()
+
+def check_unique_id(trks):
+    num_trks = len(trks)
+    ids = set([t.type_and_id for t in trks])
+    assert len(ids) == num_trks
+
+
+def call_bin(save_path):
+    import subprocess
+    print('Start evaluating bin file...')
+    ret_bytes = subprocess.check_output(
+        f'./compute_tracking_metrics_main_official {save_path} ' + './gt.bin',
+        shell=True)
+    ret_texts = ret_bytes.decode('utf-8')
+    print(ret_texts)
+    txt_path = save_path.replace('.bin', '.txt')
+    with open(txt_path, 'w') as fw:
+        fw.write(ret_texts)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config', type=str)
+parser.add_argument('--bin_path_1', type=str, default='./mot_results/waymo/validation/immortal_gpu_real3d_fsdpp/bin/pred.bin')
+parser.add_argument('--bin_path_2', type=str, default='./mot_results/waymo/validation/immortal_backward_re_fsdpp/bin/pred.bin')
+parser.add_argument('--device', type=int, default=-1)
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    bin_path_1 = './mot_results/waymo/validation/immortal_gpu_real3d_fsdpp/bin/pred.bin'
-    bin_path_2 = './mot_results/waymo/validation/immortal_backward_re_fsdpp/bin/pred.bin'
+    if args.device > -1:
+        import torch
+        torch.cuda.set_device(args.device)
+    bin_path_1 = args.bin_path_1
+    bin_path_2 = args.bin_path_2
     bin_data_1 = read_bin(bin_path_1)
     bin_data_2 = read_bin(bin_path_2)
+    save_name = osp.basename(args.config).split('.')[0]
+    save_path = osp.join(osp.dirname(bin_path_1), save_name + '.bin')
+    print(f'Results will be saved to {save_path}')
 
     tracklet_dict_1 = generate_tracklets(bin_data_1)
     tracklet_dict_2 = generate_tracklets(bin_data_2)
 
+
+    out_tracklet_dict = {}
+
     assert set(tracklet_dict_1.keys()) == set(tracklet_dict_2.keys()), 'It is almost impossible that a sequence does not contain any tracklet'
 
-    cfg = {'max_distance':10}
-    for segment_name, trks_1 in tracklet_dict_1.items():
+    cfg = yaml.load(open(args.config, 'r'))
+    print('Start match and merge...')
+    for segment_name, trks_1 in tqdm(tracklet_dict_1.items()):
         trks_2 = tracklet_dict_2[segment_name]
-        indices = match_tracklets(trks_1, trks_2, mode='center_distance', cfg=cfg)
+        pairs = match_tracklets(trks_1, trks_2, cfg=cfg)
+        merged_trks = merge_tracklets(trks_1, trks_2, pairs, cfg)
+        check_unique_id(merged_trks)
+        out_tracklet_dict[segment_name] = merged_trks
+    
+    save_to_bin(out_tracklet_dict, save_path)
+    call_bin(save_path)
--- mot_3d/tracklet/simple_tracklet.py
+++ mot_3d/tracklet/simple_tracklet.py
@@ -1,5 +1,8 @@
 import numpy as np
 from ipdb import set_trace
+from waymo_open_dataset import dataset_pb2
+from waymo_open_dataset import label_pb2
+from waymo_open_dataset.protos import metrics_pb2
 
 class SimpleTracklet(object):
 
@@ -14,17 +17,21 @@ class SimpleTracklet(object):
 
         self.uuid = uuid
         assert '-' in uuid
-        self.segment_name = uuid.split('-')[0]
+        self.segment_name, self.type_and_id = uuid.split('-')
         self.type = type
+        checktype, int_id = self.type_and_id.split('_')
+        self.int_id = int(int_id)
+        assert isinstance(self.type, int)
+        assert int(checktype) == self.type
         self.size = len(self.box_list)
 
     
-    def append(self, box, ts, id=None):
+    def append(self, box, ts, uuid=None):
         self.box_list.append(box)
         self.ts_list.append(ts)
         self.size += 1
-        if id is not None:
-            assert self.id == id
+        if uuid is not None:
+            assert self.uuid == uuid
     
     def freeze(self):
         self.ts2index = {ts:i for i, ts in enumerate(self.ts_list)}
@@ -43,28 +50,148 @@ class SimpleTracklet(object):
         return self.size
     
     @classmethod
+    def boxes2mm(cls, boxes):
+        boxes = boxes[:, :7].copy()
+        heading = -boxes[:, 6] - 0.5 * np.pi
+        
+        while (heading < -np.pi).any():
+            heading[heading < -np.pi] += 2 * np.pi
+
+        while (heading > np.pi).any():
+            heading[heading > np.pi] -= 2 * np.pi
+
+        boxes[:, 6] = heading
+
+        return boxes
+    
+    @classmethod
     def overlap_ts(cls, trk1, trk2):
         s1 = set(trk1.ts_list)
         s2 = set(trk2.ts_list)
         inter = s1.intersection(s2)
         # union = s1.union(s2)
-        unq1 = sorted(list(s1 - s2))
-        unq2 = sorted(list(s2 - s1))
-        overlap_list = sorted(list(inter))
-        return overlap_list, unq1, unq2
+        return inter, s1 - s2, s2 - s1
     
     @classmethod
-    def tracklet_distance(cls, trk1, trk2, mode='center_distance', cfg=None):
-        return getattr(cls, mode)(trk1, trk2, cfg)
+    def tracklet_distance(cls, trk1, trk2, cfg):
+        return getattr(cls, cfg['mode'])(trk1, trk2, cfg)
     
     @classmethod
     def center_distance(cls, trk1, trk2, cfg):
         overlap_ts, unq_ts1, unq_ts2 = cls.overlap_ts(trk1, trk2)
+        overlap_ts = sorted(list(overlap_ts))
+        unq_ts1 = sorted(list(unq_ts1))
+        unq_ts2 = sorted(list(unq_ts2))
         num_unq = len(unq_ts1) + len(unq_ts2)
-        if len(overlap_ts) == 0:
+        min_overlap = cfg.get('min_overlap', 1)
+        if len(overlap_ts) < min_overlap:
             return cfg['max_distance']
         boxes_1 = np.stack([trk1[ts] for ts in overlap_ts], 0)
         boxes_2 = np.stack([trk2[ts] for ts in overlap_ts], 0)
-        center_dist = np.linalg.norm(boxes_1[:, :3] - boxes_2[:, :3], ord=2, axis=1)
-        avg_dist = (center_dist.sum().item() + num_unq * cfg['max_distance']) / (num_unq + len(overlap_ts))
+        if cfg.get('with_dimensions', False):
+            center_dist = np.linalg.norm(boxes_1[:, :6] - boxes_2[:, :6], ord=2, axis=1)
+        else:
+            center_dist = np.linalg.norm(boxes_1[:, :3] - boxes_2[:, :3], ord=2, axis=1)
+        # avg_dist = (center_dist.sum().item() + num_unq * cfg['max_distance']) / (num_unq + len(overlap_ts))
+        avg_dist = center_dist.mean()
         return avg_dist
+
+    @classmethod
+    def iou3d(cls, trk1, trk2, cfg):
+        from mot_3d.utils.cuda_ops import calculate_iou_3d_gpu
+        overlap_ts, unq_ts1, unq_ts2 = cls.overlap_ts(trk1, trk2)
+        overlap_ts = sorted(list(overlap_ts))
+        unq_ts1 = sorted(list(unq_ts1))
+        unq_ts2 = sorted(list(unq_ts2))
+        num_unq = len(unq_ts1) + len(unq_ts2)
+        min_overlap = cfg.get('min_overlap', 1)
+        if len(overlap_ts) < min_overlap:
+            return cfg['max_distance']
+        boxes_1 = np.stack([trk1[ts] for ts in overlap_ts], 0)
+        boxes_2 = np.stack([trk2[ts] for ts in overlap_ts], 0)
+        center_dist = np.linalg.norm(boxes_1[:, :3] - boxes_2[:, :3], ord=2, axis=1).mean().item()
+
+        if center_dist > 20:
+            ious = np.zeros(len(boxes_1))
+        else:
+            boxes_1 = cls.boxes2mm(boxes_1)
+            boxes_2 = cls.boxes2mm(boxes_2)
+            iou_matrix = calculate_iou_3d_gpu(boxes_1, boxes_2)
+            ious = np.diagonal(iou_matrix)
+
+        dist = 1 - ious
+        avg_dist = dist.mean()
+        return avg_dist
+    
+    @classmethod
+    def weighted_box_fuse(cls, box1, box2, cfg):
+        s1, s2 = box1[7], box2[7]
+        new_box = (box1 * s1 + box2 * s2) / (s1 + s2)
+        return new_box
+
+    @classmethod
+    def max_box_fuse(cls, box1, box2, cfg):
+        s1, s2 = box1[7].item(), box2[7].item()
+        if s1 > s2:
+            return box1
+        else:
+            return box2
+
+    @classmethod
+    def first_priority_fuse(cls, box1, box2, cfg):
+        return box1
+
+    @classmethod
+    def merge_tracklets(cls, trks, cfg):
+        # two trks for now
+        trk1, trk2 = trks[0], trks[1]
+        overlap_ts, unq_ts1, unq_ts2 = cls.overlap_ts(trk1, trk2)
+        all_ts = sorted(list(overlap_ts.union(unq_ts1).union(unq_ts2)))
+        out_trk = SimpleTracklet(trk1.uuid, trk1.type)
+        for ts in all_ts:
+            if ts in overlap_ts:
+                box1, box2 = trk1[ts], trk2[ts]
+                box_fuse_func = cfg.get('box_fuse_func', None)
+                if box_fuse_func is not None:
+                    new_box = getattr(cls, box_fuse_func)(box1, box2, cfg)
+                else:
+                    new_box = (box1 + box2) / 2
+                out_trk.append(new_box, ts)
+                pass
+            elif ts in unq_ts1:
+                box1 = trk1[ts]
+                out_trk.append(box1, ts)
+            else:
+                box2 = trk2[ts]
+                out_trk.append(box2, ts)
+        return out_trk
+    
+    def waymo_objects(self,):
+        out_list = []
+        for box_np, ts in zip(self.box_list, self.ts_list):
+            o = metrics_pb2.Object()
+            o.context_name = self.segment_name
+            o.frame_timestamp_micros = ts
+            box = label_pb2.Label.Box()
+
+            box.center_x, box.center_y, box.center_z, box.width, box.length, box.height, box.heading, o.score = box_np.tolist()
+            o.object.box.CopyFrom(box)
+
+            # meta_data = label_pb2.Label.Metadata()
+            # if args.velo:
+            #     meta_data.speed_x, meta_data.speed_y = velo[0], velo[1]
+            # if args.accel:
+            #     meta_data.accel_x, meta_data.accel_y = accel[0], accel[1]
+            # o.object.metadata.CopyFrom(meta_data)
+
+            o.object.id = self.type_and_id
+            o.object.type = self.type
+            out_list.append(o)
+        return out_list
+    
+    def increase_id(self, base):
+        new_id = self.int_id + base
+        self.int_id = new_id
+        self.type_and_id = str(self.type) + '_' + str(new_id)
+        self.uuid = self.segment_name + '-' + self.type_and_id
+        
